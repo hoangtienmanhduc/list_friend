e9116543f8d7b91c97750846f249097180b336b5
Hoang-Tien-Manh-Duc <ducdeveloper@1201@gmail.com>
2025-02-21 21:56:41 +0700

    feat: setup project

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :friends
+  # get "home/index"
+  get "home/about"
+  root "home#index"
+  get "up" => "rails/health#show", as: :rails_health_check
+
+end
